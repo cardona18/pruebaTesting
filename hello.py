223a43059f9af0916cf7d9a5daf7246b4a1f0969
cardona18 <46799955+cardona18@users.noreply.github.com>
2019-03-12 12:31:05 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 print("Hello world!")
+numero = int(input("Escriba un número positivo: "))
+if numero < 0:
+    print("¡Le he dicho que escriba un número positivo!")
+print(f"Ha escrito el número {numero}")
